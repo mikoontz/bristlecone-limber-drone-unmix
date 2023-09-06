7597495aef888f7c000e9f2219cd03d8ddb97990
swnordstrom <swnordstrom@email.wm.edu>
2023-09-06 10:16:04 -0700

    mod field survey download script (take out filename, add train/test)

--- src/download_field_survey_data.R
+++ src/download_field_survey_data.R
@@ -1,12 +1,14 @@
 #' @description 
-#' Function will download all field survey data
+#' Function will download field survey data. Currently set up for only aggie air products.
 #' 
 #' @param species
-#' Species to read in (in our implementation, one of PILO, PIEN, PIFL, firs, dead)
-#' @param filetype
-#' Filetype to read in (in our implementation, one of cpg, dbf, prj, sbn, sbx, shp, shpxml, shx)
+#' Species to read in (in our implementation, one of PILO, PIEN, PIFL, firs, dead, as string)
+#' @param set
+#' Test or train data set (in our implementation, test or train, as string)
+#' 
+#' @returns data frame with local filepaths to downloaded files
 
-download_field_survey_data <- function(species = NULL, filetype = NULL) {
+download_field_survey_data <- function(species = NULL, set = NULL) {
   
   # Check to make sure function for downloading google drive data is sourced
   # (if not, read it in)
@@ -22,6 +24,8 @@ download_field_survey_data <- function(species = NULL, filetype = NULL) {
   # This function is written to handle the following species codes:
   #    - PILO, PIEN, PIFL
   #    - firs, dead
+  # The following dataset types:
+  #    - train, test
   # This function is also written to handle the following filetypes:
   #    - cpg, dbf, prj, sbn, sbx, shp, shpxml, shx
   # It also assumes a specific coding in the data-links.csv file
@@ -29,22 +33,21 @@ download_field_survey_data <- function(species = NULL, filetype = NULL) {
   
   # Get only relevant files (coded beginning with species names)
   data_links <- data_links |>
-    dplyr::filter(
-      grepl('^PI', object) | grepl('^firs', object) | grepl('^dead', object)
-    )
+    dplyr::filter(grepl('aggie', object))
   
   # Go by cases
   # Default case is ALL
   
-  if (!is.null(species) | !is.null(filetype)) {
+  if (!is.null(species) | !is.null(set)) {
     
     # If species or filetype is specified,  modify the dataframe for easy matching
     data_links <- data_links |>
       dplyr::mutate(split.temp = gsub('train', '_train_', object)) |>
       dplyr::mutate(split.temp = gsub('test', '_test_', split.temp)) |>
-      tidyr::separate(col = split.temp, into = c("sp", "train", "ft"), sep = '__')
+      dplyr::mutate(split.temp = gsub('aggieair_', '', split.temp)) |>
+      tidyr::separate(col = split.temp, into = c("sp", "st", "ft"), sep = '__')
     
-    if (!is.null(species) & is.null(filetype)) {
+    if (!is.null(species) & is.null(set)) {
       
       # Get a regex string for subsetting
       regex_string <- paste(species, collapse = '|^')
@@ -53,13 +56,13 @@ download_field_survey_data <- function(species = NULL, filetype = NULL) {
       # Subset data_links data frame using above specified 
       data_links <- data_links |> dplyr::filter(grepl(regex_string, sp))
     
-    } else if (!is.null(filetype) & is.null(species)) {
+    } else if (!is.null(set) & is.null(species)) {
       
-      regex_string <- paste(filetype, collapse = '$|^')
+      regex_string <- paste(set, collapse = '$|^')
       # Add caret to beginning and dollar sign to end
       regex_string <- paste0('^', regex_string, '$')
       # Subset data_links using above specified
-      data_links <- data_links |> dplyr::filter(grepl(regex_string, ft))
+      data_links <- data_links |> dplyr::filter(grepl(regex_string, st))
     
     } else {
     
@@ -67,13 +70,13 @@ download_field_survey_data <- function(species = NULL, filetype = NULL) {
       # Add caret to beginning
       sp_regex_string <- paste0('^', sp_regex_string)
       
-      ft_regex_string <- paste(filetype, collapse = '$|^')
+      set_regex_string <- paste(set, collapse = '$|^')
       # Add caret to beginning and dollar sign to end
-      ft_regex_string <- paste0('^', ft_regex_string, '$')
+      set_regex_string <- paste0('^', set_regex_string, '$')
       
       data_links <- data_links |>
         dplyr::filter(grepl(sp_regex_string, sp)) |>
-        dplyr::filter(grepl(ft_regex_string, ft))
+        dplyr::filter(grepl(set_regex_string, st))
       
     }
       
